c59d21017c7cf9f2d4880ac94086f4a9db38f779
Anthony <18110175@student.hcmute.edu.vn>
2023-07-19 03:49:48 +0700

    Init maven project

---
+++ simpleauth/src/main/java/tech/dinhphu28/simpleauth/SimpleauthApplication.java
@@ -0,0 +1,13 @@
+package tech.dinhphu28.simpleauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleauthApplication.class, args);
+	}
+
+}
---
+++ simpleauth/src/test/java/tech/dinhphu28/simpleauth/SimpleauthApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.dinhphu28.simpleauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
