9e222cedc91239ca3eea3e540fa315febdf4b7c9
Anthony <18110175@student.hcmute.edu.vn>
2023-07-20 16:59:43 +0700

    Add AuthenticationResponse.java

---
+++ simpleauth/src/main/java/tech/dinhphu28/simpleauth/models/AuthenticationResponse.java
@@ -0,0 +1,19 @@
+package tech.dinhphu28.simpleauth.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthenticationResponse {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+}
