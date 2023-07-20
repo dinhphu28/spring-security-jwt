88f1051007e2325a785f599ca69e2400ca5241a6
Anthony <18110175@student.hcmute.edu.vn>
2023-07-20 09:02:31 +0700

    Add Token.java

---
+++ simpleauth/src/main/java/tech/dinhphu28/simpleauth/entities/Token.java
@@ -0,0 +1,30 @@
+package tech.dinhphu28.simpleauth.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Token {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column(unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    private TokenType tokenType = TokenType.BEARER;
+
+    private boolean revoked;
+
+    private boolean expired;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
