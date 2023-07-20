c2a628be838b1d052ff1a4839a90cd815497cd8f
Anthony <18110175@student.hcmute.edu.vn>
2023-07-20 17:00:43 +0700

    Fix: Update Permission.java
    Enum ADMIN_DELETE with value "admin:create" -> "admin:delete"

--- simpleauth/src/main/java/tech/dinhphu28/simpleauth/entities/Permission.java
+++ simpleauth/src/main/java/tech/dinhphu28/simpleauth/entities/Permission.java
@@ -8,7 +8,7 @@ public enum Permission {
     ADMIN_READ("admin:read"),
     ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
-    ADMIN_DELETE("admin:create"),
+    ADMIN_DELETE("admin:delete"),
     MANAGER_READ("management:read"),
     MANAGER_UPDATE("management:update"),
     MANAGER_CREATE("management:create"),
