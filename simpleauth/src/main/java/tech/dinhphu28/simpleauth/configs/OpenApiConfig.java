8f15411bf7835a46c644a7054bc1787fc77280bf
Anthony <18110175@student.hcmute.edu.vn>
2023-07-20 16:58:16 +0700

    Add OpenApiConfig.java

---
+++ simpleauth/src/main/java/tech/dinhphu28/simpleauth/configs/OpenApiConfig.java
@@ -0,0 +1,54 @@
+package tech.dinhphu28.simpleauth.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Dinh Phu",
+                        email = "contact@dinhphu28.tech",
+                        url = "https://www.dinhphu28.tech"
+                ),
+                description = "OpenApi documentation for Spring Security",
+                title = "OpenApi specification - dinhphu28",
+                version = "1.0",
+                license = @License(
+                        name = "License name",
+                        url = "https://some-url.com"
+                ),
+                termsOfService = "Terms of service"
+        ),
+        servers = {
+                @Server(
+                        description = "Local ENV",
+                        url = "http://localhost:8080"
+                ),
+                @Server(
+                        description = "PROD ENV",
+                        url = "https://aliboucoding.com/course"
+                )
+        },
+        security = {
+                @SecurityRequirement(
+                        name = "bearerAuth"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenApiConfig {
+}
